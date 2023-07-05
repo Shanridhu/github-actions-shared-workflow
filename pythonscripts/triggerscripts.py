2276667f810c88499019220383bbcf3f95bd83c0
Shanridhu <93445613+Shanridhu@users.noreply.github.com>
2023-07-05 18:14:07 +0530

    Update triggerscripts.py

--- pythonscripts/triggerscripts.py
+++ pythonscripts/triggerscripts.py
@@ -3,16 +3,16 @@ import os
 import requests
 import sys
 
-TOKEN= str(sys.argv[0])
-OWNER= str(sys.argv[1])
-REPO= str(sys.argv[2])
-Workflow_Name= str(sys.argv[3])
-pl_Baseline_Number= str(sys.argv[4])
-pl_Baseline_Revision = str(sys.argv[5])
+TOKEN = str(sys.argv[0])
+OWNER = str(sys.argv[1])
+REPO = str(sys.argv[2])
+Workflow_Name = str(sys.argv[3])
+payload_Baseline_Number = str(sys.argv[4])
+payload_Baseline_Revision = str(sys.argv[5])
 
 
 print( "the toke value is")
-def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
+def trigger_workflow(Workflow_Name,payload_Baseline_Number,payload_Baseline_Revision):
 
       headers = {
         "Accept": "application/vnd.github.v3+json",
@@ -22,8 +22,8 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
       data = {
         "event_type": Workflow_Name,
         "client_payload": {
-          'baselinetag': pl_Baseline_Number,
-          'revision_number': pl_Baseline_Revision
+          'baselinetag': payload_Baseline_Number,
+          'revision_number': payload_Baseline_Revision
         }
       }
 
@@ -36,4 +36,4 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
       print("The respoinse message is ",responsevalue.content)
 
 
-trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
+trigger_workflow(Workflow_Name,payload_Baseline_Number,payload_Baseline_Revision)
